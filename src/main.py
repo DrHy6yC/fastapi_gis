a7210fd59cef6824bfee1cad164782204a6fea45
Denis Plaksin <dr.hy6yc@gmail.com>
2025-07-04 11:47:45 +0300

    Добавление основы проекта. Обновил .gitignore. Обновил README.md Добавил файл с зависимостями requirements.txt. Установил форматер, линтер и типизатор.

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
