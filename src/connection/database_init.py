eea441f491d6bbec12d429821712db34b8c338b7
Denis Plaksin <dr.hy6yc@gmail.com>
2025-07-04 13:05:30 +0300

    Рефакторинг кода

--- src/connection/database_init.py
+++ src/connection/database_init.py
@@ -1,9 +1,7 @@
 from sqlalchemy import NullPool
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
-
 from src.config import settings
-
 
 engine = create_async_engine(
     url=settings.db_url,
