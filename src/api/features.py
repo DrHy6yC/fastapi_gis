a4fb345ea12be05ae529a770d37aa58f2fcdd975
Denis Plaksin <dr.hy6yc@gmail.com>
2025-07-04 12:32:32 +0300

    Добавление основные эндпоинты для добавления(POST /features), удаления(DELETE /features) объекта и вывода всех имеющихся объектов(GET /features), a так же статистики (GET /stats) по типам объектов. Добавил примеры принимаемых данных.

---
+++ src/api/features.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/features", tags=["Управление геометрией"])
+
+
+@router.post(path="/", summary="Добавление объекта")
+async def create_feature():
+    pass
+
+
+@router.get(path="/", summary="Получение всех объектов")
+async def get_all_features():
+    pass
+
+
+@router.delete(path="/", summary="Удаление объекта")
+async def delete_feature():
+    pass
---
+++ src/api/stats.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/stats", tags=["Статистика"])
+
+
+@router.get(path="/", summary="Получить статистику по типам")
+async def get_stats():
+    pass
