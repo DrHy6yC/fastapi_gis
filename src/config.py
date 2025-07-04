ea8a73e4b37cb1fc3658550fd47495f60004abcd
Denis Plaksin <dr.hy6yc@gmail.com>
2025-07-04 13:03:33 +0300

    Добавил БД Postgres. Добавил конфиг с подключением к бд из .env. Добавил контейнер с БД. Добавил engin, session maker, BaseORM для работы в FastApi. Добавил настройки для форматера, линтера и типизатора.

---
+++ src/config.py
@@ -0,0 +1,34 @@
+from typing import Literal
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # Настройка среды
+    MODE: Literal["TEST", "LOCAL", "DEV", "PROD"] = Field(default="LOCAL")
+
+    # Настройки БД
+    PG_HOST: str = Field(default="")
+    PG_PORT: int = Field(default=0)
+    PG_USER: str = Field(default="")
+    PG_PASSWORD: str = Field(default="")
+    PG_DB_NAME: str = Field(default="")
+
+    @property
+    def db_url(self) -> str:
+        """
+        Возвращает строку подключения к БД
+        :return: str
+        """
+        return (
+            f"postgresql+asyncpg:"
+            f"//{self.PG_USER}:{self.PG_PASSWORD}@"
+            f"{self.PG_HOST}:{self.PG_PORT}/{self.PG_DB_NAME}"
+        )
+
+    # Настройки откуда будут браться данные переменных окружения
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
---
+++ src/connection/database_init.py
@@ -0,0 +1,29 @@
+from sqlalchemy import NullPool
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from src.config import settings
+
+
+engine = create_async_engine(
+    url=settings.db_url,
+    echo=False,
+)
+engine_null_pool = create_async_engine(
+    url=settings.db_url,
+    echo=False,
+    pool_class=NullPool,
+)
+
+async_session_maker = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=False,
+)
+async_session_maker_null_pool = async_sessionmaker(
+    bind=engine_null_pool,
+    expire_on_commit=False,
+)
+
+
+class BaseORM(DeclarativeBase):
+    pass
