2b899367c37a0732fae08b905aea67f5246a8ac9
denisigorevich <dr.hy6yc@gmail.com>
2025-07-05 06:34:07 +0300

    Исправил тип возвращаемых данных для статистики. В swagger корректный тип возвращаемых значений

--- src/api/stats.py
+++ src/api/stats.py
@@ -5,5 +5,5 @@ router = APIRouter(prefix="/stats", tags=["Статистика"])
 
 
 @router.get(path="", summary="Получить статистику по типам")
-async def get_stats(db: DBDep) -> dict:
+async def get_stats(db: DBDep) -> dict[str, int]:
     return await db.feature.get_feature_count_by_type()
