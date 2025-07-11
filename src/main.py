c73235065a5c00997445960dc1746abc2b6df438
Denis Plaksin <dr.hy6yc@gmail.com>
2025-07-11 17:51:02 +0300

    Настраиваю подгрузку статических файлов

--- src/main.py
+++ src/main.py
@@ -10,8 +10,9 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.api.features import router as features_router
 from src.api.stats import router as stats_router
@@ -22,11 +23,22 @@ async def lifespan(app: FastAPI):
     yield
 
 
+class RootPathMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        forwarded_prefix = request.headers.get("x-forwarded-prefix")
+        if forwarded_prefix:
+            request.scope["root_path"] = forwarded_prefix
+        return await call_next(request)
+
+
 app = FastAPI(lifespan=lifespan, root_path=settings.ROOT_PATH)
+
+app.add_middleware(RootPathMiddleware)
+
 app.include_router(features_router)
 app.include_router(stats_router)
 
-app.mount(f"{settings.ROOT_PATH}/static", StaticFiles(directory="src/static"), name="static")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 
 if __name__ == "__main__":
